4eaa1c274ffcc4e8b87101bfd8a2cd388126a0f1
Ufere <uferejames@gmail.com>
2022-09-26 16:30:06 -0700

    Correct Copying memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,23 +1,19 @@
 #include "main.h"
+
 /**
- *_memcpy -   copy number bytes form adress "from" to adress "to"
- *@dest: content is to be copied
- *@src: Source of data to be copied
- *@n: bytes of the memory
- *Return: dest
+ * _memcpy - memset function
+ * @dest: start point of string to change
+ * @src: value that will replace
+ * @n: number of bytes to change
+ * Return: changed pointer
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	char *dest1 = dest;
+	char *src1 = src;
 
-	i = 0;
-	while (i < n)  /*Delacring WHILE*/
-	{
-		(dest + i) = *(src + i); /*add 1 position dest and src*/
-		i++;
-	} /*END WHILE*/
-
+	for (; n != 0; dest1++, n--, src1++)
+		*dest1 = *src1;
 	return (dest);
 }
-
