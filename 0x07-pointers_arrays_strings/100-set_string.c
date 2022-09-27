a684aefc2d6b73d8195c683f8bdbe0913cff4bf8
Ufere <uferejames@gmail.com>
2022-09-26 17:08:20 -0700

    setting the value of a pointer to a char.

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,22 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_diagsums - add diagonal lines of a square matrix
- * @a: square matrix
- * @size: size of matrix
+ * set_string - function that sets the value of a pointer to a char
+ * @s: value to overide
+ * @to: value to copy
+ *
+ *
+ *
  */
-
-void print_diagsums(int *a, int size)
+void set_string(char **s, char *to)
 {
-	int lfrg = 0, rglf = size - 1;
-	int valuelfrg = 0, valuerglf = 0;
-	int *p = a;
-
-	for (; lfrg <= size * size; lfrg += (size + 1), rglf += (size - 1))
-	{
-		valuelfrg += *(p + lfrg);
-		valuerglf += *(p + rglf);
-	}
-	printf("%d, %d\n", valuelfrg, valuerglf);
+	*s = to;
 }
